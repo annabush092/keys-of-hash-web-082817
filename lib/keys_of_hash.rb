245175c4396138cfbc3bb525f35f9d1ff08d3e78
Anna Bush <annabush092@gmail.com>
2017-08-19 15:07:19 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,15 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    matching_keys = []
+    #arguments contains an array of values to compare to the hash values this is called on
+    #find all matching values and add their keys to the matching_keys array
+    arguments.each do |test_value|
+      self.each do |key, hash_value|
+        if test_value == hash_value
+          matching_keys << key
+        end
+      end
+    end
+    matching_keys
   end
 end
